a553ae07aae977e43391733ea430df26e469debb
Odzen <jsebastian.va@gmail.com>
2023-10-06 15:15:40 -0600

    user tokens .env

--- src/config/jwt.py
+++ src/config/jwt.py
@@ -6,6 +6,9 @@ class JWTSettings(BaseSettings):
 
     jwt_secret_key: str
     jwt_encoding_algorithm: str
+    jwt_admin_token: str
+    jwt_dev_token: str
+    jwt_employee_token: str
     model_config = SettingsConfigDict(env_file=env_file, extra="ignore")
 
 
