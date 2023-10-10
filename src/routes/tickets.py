631bab06abe48fb44c942f336a7338fba042d996
Odzen <jsebastian.va@gmail.com>
2023-10-10 15:24:46 -0600

    routing mvp

--- src/routes/tickets.py
+++ src/routes/tickets.py
@@ -1,5 +1,5 @@
-from src.schemas.requests import RequestRetrieved, UpdateRequest, CreateRequest, TypeRequest
-from src.services.requests import RequestsService
+from src.schemas.tickets import CreateTicket, TicketRetrieved, UpdateTicket
+from src.services.tickets import TicketsService
 from fastapi import APIRouter, Body, status, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
@@ -11,3 +11,48 @@ from src.permissions.users import get_user_from_access_token, user_is_admin_or_d
 
 
 router = APIRouter()
+
+@router.get("/tickets/{id}", response_model=TicketRetrieved, tags=["tickets"])
+async def retrieve_ticket_by_id(id: PyObjectId):
+    
+    ticket = await TicketsService().get_ticket_by_Id(id)
+
+    return JSONResponse(
+        content=jsonable_encoder(ticket),
+        status_code=status.HTTP_200_OK
+    )
+
+@router.get("/tickets", response_model = Page[dict], dependencies=[Depends(get_user_from_access_token)], tags=["tickets"])
+async def get_all_tickets():
+    
+    tickets = await TicketsService().list_all_tickets()
+
+    return paginate(
+        jsonable_encoder(tickets)
+    )
+
+@router.post("/tickets", response_model = TicketRetrieved, tags=["tickets"], 
+             description="Endpoint to create a ticket. Any logged user can perform this action.")
+async def create_ticket(ticket_to_create: CreateTicket = Body(), user: User = Depends(get_user_from_access_token)):
+    
+    ticket = await TicketsService().create_ticket(ticket_to_create=ticket_to_create, user_creator=user)
+
+    return JSONResponse(
+        content = jsonable_encoder(ticket),
+        status_code = status.HTTP_200_OK
+    )
+
+    
+@router.patch("/tickets/{ticket_id}", response_model = TicketRetrieved, 
+              tags = ["ticket"],
+              description="Endpoint to update a ticket by ID. Only creators of the ticket can perform this action or admins.")
+async def update_ticket_by_Id(ticket_id: PyObjectId, 
+                               user_ticketing=Depends(user_is_admin_or_dev),
+                               updated_ticket: UpdateTicket = Body()):
+    
+    updated_ticket = await TicketsService().update_ticket_by_Id(ticket_id, updated_ticket, user_ticketing)
+
+    return JSONResponse(
+        content=jsonable_encoder(updated_ticket),
+        status_code=status.HTTP_200_OK
+    )
